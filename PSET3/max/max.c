5b674a30b57326b53da885a05d6950ebfc78fc4f
Lucca Chiguti <116118085+mikufan3939@users.noreply.github.com>
2024-05-14 01:05:57 -0300

    Week 3 Problem Set

---
+++ PSET3/max/max.c
@@ -0,0 +1,44 @@
+// Practice writing a function to find a max value
+
+#include <cs50.h>
+#include <stdio.h>
+
+int max(int array[], int n);
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Number of elements: ");
+    }
+    while (n < 1);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = get_int("Element %i: ", i);
+    }
+
+    printf("The max value is %i.\n", max(arr, n));
+}
+
+// TODO: return the max value
+int max(int array[], int n)
+{
+    int maxValue=0;
+
+    for(int i=0;i<n;i++)
+    {
+        if(i==0)
+        {
+            maxValue=array[i];
+        }
+        else if(array[i]>maxValue)
+        {
+            maxValue=array[i];
+        }
+    }
+    return maxValue;
+}
